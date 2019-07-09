d26f853f68cb893e5a9f14f216d4e11f2b18d94f
Arístides Caballero Alfaro <aristides@itmilpaalta.edu.mx>
2019-07-09 09:36:56 -0500

    ultima versión 09 de julio 2019

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'profesor', loadChildren: './profesor/profesor.module#ProfesorPageModule' },
+  { path: 'materias', loadChildren: './materias/materias.module#MateriasPageModule' },
+  { path: 'materias2', loadChildren: './materias2/materias2.module#Materias2PageModule' },
+  { path: 'materias3', loadChildren: './materias3/materias3.module#Materias3PageModule' },
+  { path: 'materias4', loadChildren: './materias4/materias4.module#Materias4PageModule' },
+  { path: 'grafica', loadChildren: './grafica/grafica.module#GraficaPageModule' },
+  { path: 'reporte', loadChildren: './reporte/reporte.module#ReportePageModule' },
 ];
 
 @NgModule({
